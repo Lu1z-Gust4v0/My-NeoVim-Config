c66c9aa7ead92e54a443bedf5bbb0c1ce94802db
Luiz Gustavo <gustavo1204.gc@gmail.com>
2023-03-01 12:46:55 -0300

    Define keyboard shortcuts for markdown preview

--- mappings.lua
+++ mappings.lua
@@ -7,6 +7,18 @@ M.general = {
   },
 }
 
+M.mdpreview = {
+  n = {
+    ["<leader>pw"] = { "<cmd> MarkdownPreview<CR>", "Open Markdown Preview" },
+    ["<leader>px"] = { "<cmd> MarkdownPreviewStop<CR>", "Close Markdown Preview" },
+  },
+
+  i = {
+    ["<A-p>"] = {"<cmd> MarkdownPreview<CR>" },
+    ["<A-x>"] = {"<cmd> MarkdownPreviewStop<CR>" },
+  },
+}
+
 -- more keybinds!
 
 return M
