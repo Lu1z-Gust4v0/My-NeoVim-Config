dc1d632ee7d658aab4fc8d18b0ab6e439151e105
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-03 12:15:42 -0300

    Update tab size

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+
+-- Indenting
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
