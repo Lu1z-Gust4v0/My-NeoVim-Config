bb27f4f750c32871bf06ca0b7374891f581f625f
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-09 01:11:19 -0300

    Add svelte lsp and code formatting

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,4 +1,3 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
@@ -7,6 +6,7 @@ local servers = {
   "html",
   "cssls",
   "tsserver",
+  "svelte",
   "emmet_ls",
   "eslint",
   "jsonls",
@@ -20,6 +20,11 @@ local servers = {
   "prismals",
   "gopls"
 }
+
+local on_attach = function(client, bufnr)
+    require("lsp-format").on_attach(client, bufnr)
+    require("plugins.configs.lspconfig").on_attach(client, bufnr)
+end
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -5,10 +5,11 @@ local lint = null_ls.builtins.diagnostics
 
 local sources = {
   formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  formatting.prettier,
   formatting.stylua,
   formatting.clang_format,
   formatting.black,
+  formatting.gofmt,
   lint.prettier,
   lint.revive,
   lint.golangci_lint,
