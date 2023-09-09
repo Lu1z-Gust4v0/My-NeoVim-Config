7fa1fdb6c87ad58697cc500596a87bd9451ee220
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-09 01:09:18 -0300

    Drop 4 spaces identation enforcement

--- init.lua
+++ init.lua
@@ -1,13 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
-local opt = vim.opt
-
--- Indenting
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.softtabstop = 4
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
