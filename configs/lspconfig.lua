00141141e40ffc0fc73e3fea72a4770a42ef0200
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-23 16:35:14 -0300

    Disable format on save, since it was causing a infinity save bug

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -22,7 +22,7 @@ local servers = {
 }
 
 local on_attach = function(client, bufnr)
-    require("lsp-format").on_attach(client, bufnr)
+    -- require("lsp-format").on_attach(client, bufnr)
     require("plugins.configs.lspconfig").on_attach(client, bufnr)
 end
 
